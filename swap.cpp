b1bb9ed3b7bd0678a32bebc264c5c34a5a322f71
Ashwin Bolne <ashwinbolne@gmail.com>
2021-06-02 22:44:22 +0530

    update swap logic in swap.cpp

--- swap.cpp
+++ swap.cpp
@@ -5,10 +5,13 @@ int main()
 {
     int num1 = 25;
     int num2 = 50;
-    num1 = num1 + num2;
-    num2 = num1 - num2;
-    num1 = num1 - num2;
-
+    // num1 = num1 + num2;
+    // num2 = num1 - num2;
+    // num1 = num1 - num2;
+    int temp ;
+    temp = num1;
+    num1 = num2;
+     num2 = temp;
     cout<<"Numbers after swapping: "<<num1<<" "<<num2<<endl;
     return 0;
 }
